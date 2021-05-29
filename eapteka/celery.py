bcd02590d25482cb857e64c7057815d2bddb2603
BossOfCreeps <seva-ra4is@mail.ru>
2021-05-29 20:20:28 +0700

    Update celery.py

--- eapteka/celery.py
+++ eapteka/celery.py
@@ -11,7 +11,6 @@ from celery import Celery
 from django.apps import apps
 
 from eapteka import settings
-from notification.bot import tg_notification
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eapteka.settings')
 
@@ -26,6 +25,7 @@ app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 @app.task(bind=True)
 def call_notification(self, product_id):
     from analogs.models import AnalogProduct
+    from notification.bot import tg_notification
     product = AnalogProduct.objects.get(id=product_id)
     tg_notification(product.set.user, product)
 
